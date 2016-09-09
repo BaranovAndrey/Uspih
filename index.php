3b9922666abdccc6b741850ae35cf9495e343bec
Andrey <andbarbox@gmail.com>
2016-09-09 21:08:16 +0300

    Add logo

---
+++ index.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Нерухомість Червоноград</title>
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+		<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/vendor/animate_css/animate.css">
+		<script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="https://use.fonticons.com/ffe176a3.js"></script>
+		<script src="assets/vendor/Smooth-Scrolling/jQuery.scrollSpeed.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-scrollTo/2.1.2/jquery.scrollTo.min.js"></script>
+	</head>
+	<body>
+    <div class="wrapper">
+	    <div class="logo" id="logo">
+	    	
+	    	<div class="contacts-logo fixed">
+	    		<div class="phone-left inf">
+	    			<p>(093) 74-04-717</p>
+	    			<p>(093) 18-61-222</p>
+	    		</div>
+	    		<img class="logo-img hidden" src="assets/img/logo.png">
+	    		<div class="phone-right inf">
+	    			<p>(067) 66-88-202</p>
+	    			<p>(067) 66-87-848</p>
+	    		</div>
+	    	</div>
+	    </div> 
+		
+		<div class="navMenu">
+			<nav class="navbar navbar-default navbar-fixed-top navbar-primary fixed-on-scroll">
+				<div class="container-fluid">
+				
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						</button>
+						<a id="navbar-brand" class="navbar-brand" href="#"><img src="assets/img/logo.png"></a>
+					</div>
+			    
+			    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+			       
+				        <li class="dropdown dropdown-primary" id="dropdown-primary">
+					        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Продаж<span class="caret"></span></a>
+					          	<ul class="dropdown-menu dropdown-menu-primary">
+					            	<li class="dropdown-submenu"><a tabindex="-1" href="#">Червоноград</a>
+		                    			<ul class="dropdown-menu">
+		                        			<li><a href="#">Однокімнатні квартири</a></li>
+		                   					<li><a href="#">Двокімнатні квартири</a></li>
+				                        	<li><a href="#">Трьохкімнатні квартири</a></li>
+				                        	<li><a href="#">Чотирьохкімнатні квартири</a></li>
+				                        	<li><a href="#">Будинки</a></li>
+				                        	<li><a href="#">Земельні ділянки</a></li>
+		                   				</ul>
+		                			</li>
+					            	<li role="separator" class="divider"></li>
+					            	<li class="dropdown-submenu"><a tabindex="-1" href="#">Львів</a>
+		                    			<ul class="dropdown-menu">
+		                        			<li><a href="#">Однокімнатні квартири</a></li>
+		                   					<li><a href="#">Двокімнатні квартири</a></li>
+				                        	<li><a href="#">Трьохкімнатні квартири</a></li>
+				                        	<li><a href="#">Чотирьохкімнатні квартири</a></li>
+				                        	<li><a href="#">Будинки</a></li>
+				                        	<li><a href="#">Земельні ділянки</a></li>
+		                   				</ul>
+		                			</li>
+					            	<li role="separator" class="divider"></li>
+					            	<li class="dropdown-submenu"><a tabindex="-1" href="#">Трускавець</a>
+		                    			<ul class="dropdown-menu">
+		                        			<li><a href="#">Однокімнатні квартири</a></li>
+		                   					<li><a href="#">Двокімнатні квартири</a></li>
+				                        	<li><a href="#">Трьохкімнатні квартири</a></li>
+				                        	<li><a href="#">Чотирьохкімнатні квартири</a></li>
+				                        	<li><a href="#">Будинки</a></li>
+				                        	<li><a href="#">Земельні ділянки</a></li>
+		                   				</ul>
+		                			</li>
+					            	<li role="separator" class="divider"></li>
+					            	<li class="dropdown-submenu"><a tabindex="-1" href="#">Ще одне місто</a>
+		                    			<ul class="dropdown-menu">
+		                        			<li><a href="#">Однокімнатні квартири</a></li>
+		                   					<li><a href="#">Двокімнатні квартири</a></li>
+				                        	<li><a href="#">Трьохкімнатні квартири</a></li>
+				                        	<li><a href="#">Чотирьохкімнатні квартири</a></li>
+				                        	<li><a href="#">Будинки</a></li>
+				                        	<li><a href="#">Земельні ділянки</a></li>
+		                   				</ul>
+		                			</li>
+					            	<li role="separator" class="divider"></li>
+					            	<li class="dropdown-submenu"><a tabindex="-1" href="#">І ще одне місто)</a>
+		                    			<ul class="dropdown-menu">
+		                        			<li><a href="#">Однокімнатні квартири</a></li>
+		                   					<li><a href="#">Двокімнатні квартири</a></li>
+				                        	<li><a href="#">Трьохкімнатні квартири</a></li>
+				                        	<li><a href="#">Чотирьохкімнатні квартири</a></li>
+				                        	<li><a href="#">Будинки</a></li>
+				                        	<li><a href="#">Земельні ділянки</a></li>
+		                   				</ul>
+	                				</li>				
+			      				</ul>
+			      		</li>
+			       <li class="dropdown dropdown-primary">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Оренда<span class="caret"></span></a>
+						<ul class="dropdown-menu dropdown-menu-primary">
+							<li><a href="#">Червоноград</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="#">Львів</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="#">Трускавець</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="#">Ще одне місто</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="#">І ще одне місто)</a></li>
+						</ul>
+			        </li>
+			        <li class="active" ><a id="services-link" href="#">Послуги Агентства</a></li>
+			        <li class="nav-link"><a href="#">Галерея</a></li>
+			        <li class="nav-link"><a href="#">Контакти</a></li>
+					</ul>
+			      
+			    
+			     
+			    </div><!-- /.navbar-collapse -->
+			  </div><!-- /.container-fluid -->
+			</nav>
+		</div>
+		<div class="leftPanel">
+			
+		</div>
+		<div class="centralPanel">
+			<div class="hotWrapper">	
+				<div class="hot">
+					<div class="jumbotron">
+						<p>Гаряча пропозиція!</p>
+						<p>...</p>
+						<p><a class="btn btn-primary btn-lg" href="#" role="button">Детальніше</a></p>
+					</div>
+				</div>	
+				<div class="hot">
+					<div class="jumbotron">
+						<p>Гаряча пропозиція!</p>
+						<p>...</p>
+						<p><a class="btn btn-primary btn-lg" href="#" role="button">Детальніше</a></p>
+					</div>
+				</div>	
+				<div class="hot">
+					<div class="jumbotron">
+						<p>Гаряча пропозиція!</p>
+						<p>...</p>
+						<p><a class="btn btn-primary btn-lg" href="#" role="button">Детальніше</a></p>
+					</div>
+				</div>	
+				<div class="hot">
+					<div class="jumbotron">
+						<p>Гаряча пропозиція!</p>
+						<p>...</p>
+						<p><a class="btn btn-primary btn-lg" href="#" role="button">Детальніше</a></p>
+					</div>
+				</div>	
+				<div class="hot">
+					<div class="jumbotron">
+						<p>Гаряча пропозиція!</p>
+						<p>...</p>
+						<p><a class="btn btn-primary btn-lg" href="#" role="button">Детальніше</a></p>
+					</div>
+				</div>	
+				<div class="hot">
+					<div class="jumbotron">
+						<p>Гаряча пропозиція!</p>
+						<p>...</p>
+						<p><a class="btn btn-primary btn-lg" href="#" role="button">Детальніше</a></p>
+					</div>
+				</div>	
+			</div>		
+		</div>
+		<div class="separator">
+			<p>Послуги агентства</p>
+		</div>
+
+			<div class="fixedPanel">
+				<div class="socials">
+					<div class="icon fb">
+						<a href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+					</div>
+					<div class="icon vk">
+						<a href="#"><i class="fa fa-vk" aria-hidden="true"></i></a>
+					</div>
+					<div class="icon odnokl">
+						<a href="#"><i class="fa fa-odnoklassniki-square" aria-hidden="true"></i></a>	
+					</div>
+					<div class="icon instagram">
+						<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+					</div>
+				</div>
+				<a href="#"><div class="contactButton animated shake">
+					
+				</div></a>
+			</div>
+		</div>
+		
+		<div class="services" id="services">
+			
+		</div>
+		<div class="gallery" id="gallery">
+			
+		</div>
+		<div class="comtacts" id="contacts">
+			
+		</div>
+		<div class="footer">
+			<div class="logo-footer">
+				<a href="#"><p>Успіх</p></a>
+			</div>
+			<div class="socials-footer">
+				<div class="icon fb">
+					<a href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+				</div>
+				<div class="icon vk">
+					<a href="#"><i class="fa fa-vk" aria-hidden="true"></i></a>
+				</div>
+				<div class="icon odnokl">
+					<a href="#"><i class="fa fa-odnoklassniki-square" aria-hidden="true"></i></a>	
+				</div>
+				<div class="icon instagram">
+					<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+				</div>				
+			</div>
+			<div class="phone-footer">
+				<p>(093) 74-04-717</p>
+    			<p>(093) 18-61-222</p>
+    			<p>(067) 66-88-202</p>
+    			<p>(067) 66-87-848</p>
+			</div>	
+		</div>
+
+	</div>
+   
+    <script>
+    	$('.navbar-primary').find('li').removeClass('active');
+    	
+    	$('.navbar-brand').addClass('hidden');
+
+	  	var logo = 200;
+
+    	window.onscroll = function() {
+    	
+    	var position = window.pageYOffset || document.documentElement.scrollTop;
+	      
+	      if (position < logo) {
+	       
+	        $('.navbar-brand').addClass('hidden');
+	        $('.contacts-logo').removeClass('fixed');
+	        $('.logo-img').removeClass('hidden');
+	        $('.navbar').removeClass('fixed-on-scroll');
+	      } 
+
+	      else if (position > logo) {
+	    	
+	    	$('.navbar-brand').removeClass('hidden');
+	        $('.contacts-logo').addClass('fixed');
+	        $('.logo-img').addClass('hidden');
+	        $('.navbar').addClass('fixed-on-scroll');
+	      }
+	    };
+    
+    	setInterval(function()
+    	{
+    		$('.contactButton').removeClass('animated shake');
+    		setTimeout(function(){
+    			$('.contactButton').addClass('animated shake');
+    		}, 0);
+    	}, 20000);
+
+
+    	$(function() {  
+
+		    jQuery.scrollSpeed(100, 800);    //Smooth page scrolling.
+		});
+
+
+		jQuery(document).ready(function(){
+
+    		jQuery('#services-link').click(function() {
+
+        		jQuery.scrollTo('#services');
+
+    		});
+
+		});
+
+       
+		
+    </script>
+	</body>
+</html>
